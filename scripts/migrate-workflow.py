b7fbaed58baa63418b37a3a640c3051891f55b99
Tim <code@tim.life>
2026-01-09 20:00:00 -0800

    chore: update SPDX header tooling scripts with auto-commit changes

--- scripts/migrate-workflow.py
+++ scripts/migrate-workflow.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# SPDX-FileCopyrightText: Copyright (c) 2025 provide.io llc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """
 Migration script to help convert existing CI workflows to use shared actions.
 
